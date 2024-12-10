afcf05db338af0bb50c1c1b957d3c487fbbf969c
Vijay Kodru <123325232+vijaykodru@users.noreply.github.com>
2024-12-10 23:29:16 +0000

    update 3

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
 function fibonacci(n) {
     function fibHelper(n, n0, n1) {
-      if (n0 === 0) return n0;
-      if (n1 === 1) return n1;
-      return fibHelper(n - 1, n1, n0 + n1);
+        if (n === 0) return n0;  
+        if (n === 1) return n1;  
+        return fibHelper(n - 1, n1, n0 + n1);  
     }
-  
-    return fibHelper(n, 0, 1);
-  }
+
+    return fibHelper(n, 0, 1);  
+}
