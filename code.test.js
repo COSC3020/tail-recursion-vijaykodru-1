c9579938808572da6e90fb70fd7d719688f5dcad
Vijay Kodru <123325232+vijaykodru@users.noreply.github.com>
2024-12-10 23:26:57 +0000

    test code update. forgot to change the function name

--- code.test.js
+++ code.test.js
@@ -2,16 +2,16 @@ const fs = require("fs");
 const assert = require("assert");
 eval(fs.readFileSync('code.js').toString());
 
-equate1 = tailrecursiveFIB(2);
+equate1 = fibonacci(2);
 result1 = 1;
 assert(equate1 == result1);
 
 
-assert(tailrecursiveFIB(3) == 2);
-assert(tailrecursiveFIB(4) == 3);
-assert(tailrecursiveFIB(5) == 5);
-assert(tailrecursiveFIB(7) == 13);
-assert(tailrecursiveFIB(9) == 34);
-assert(tailrecursiveFIB(10) == 55);
-assert(tailrecursiveFIB(15) == 610);
-assert(tailrecursiveFIB(25) == 75025);
+assert(fibonacci(3) == 2);
+assert(fibonacci(4) == 3);
+assert(fibonacci(5) == 5);
+assert(fibonacci(7) == 13);
+assert(fibonacci(9) == 34);
+assert(fibonacci(10) == 55);
+assert(fibonacci(15) == 610);
+assert(fibonacci(25) == 75025);
